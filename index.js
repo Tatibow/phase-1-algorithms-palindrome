689650c6b4dab9efd15cb3dcf59878badc9a3666
Tatiana <Bowmantatiana2@gmail.com>
2022-12-16 19:19:20 -0800

    commit tree d276f8897de712b2d44550ab704d1f8392f26910

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const wordReversed = word.split("").reverse().join("");
+
+  return word === wordReversed ? true : false;
+
+  // return word === word.split("").reverse().join("") ? true : false;
 }
 
-/* 
+/*
   Add your pseudocode here
+   if the word is the same forward and backword return true else return false
+   1. split up the word
+   2. reverse it
+   3. join it back together as a string
+   4. compare and return the answer
 */
 
 /*
   Add written explanation of your solution here
+  a palindrome is just a word being the same in reverse so I figured if I spilt it into an array I would be able to
+  use the reverse method on it and then just join it back together as a string. I then used ternary instead of an if statment
+  for cleaner code ).
 */
 
 // You can run `node index.js` to view these console logs
